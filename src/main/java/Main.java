76a6b8fe363a74f88a73a17f9ae15b7259c836c6
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2017-04-20 15:31:50 +0200

    feat(usage): prints usage on start

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,12 @@
  */
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Ejjejjj");
+    System.out.println("Python Todo application");
+    System.out.println("=======================");
+    System.out.println("Command line arguments:");
+    System.out.println(" -l   Lists all the tasks");
+    System.out.println(" -a   Adds a new task");
+    System.out.println(" -r   Removes an task");
+    System.out.println(" -c   Completes an task");
   }
 }
